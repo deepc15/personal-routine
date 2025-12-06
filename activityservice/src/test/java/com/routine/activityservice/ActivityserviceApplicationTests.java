005d17984ea7d33a9267dd47d387ececfbc9df2d
Deepsoumya Chakraborti <cdeepsoumya@gmail.com>
2025-12-07 05:03:04 +0530

    v1.02

---
+++ activityservice/src/test/java/com/routine/activityservice/ActivityserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.routine.activityservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActivityserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
