993c2778d2ba4cef83f02ec29fbc44062fe3975f
Deepsoumya Chakraborti <cdeepsoumya@gmail.com>
2025-12-07 05:47:13 +0530

    v1.03

---
+++ activityservice/src/main/java/com/routine/activityservice/model/ActivityType.java
@@ -0,0 +1,19 @@
+package com.routine.activityservice.model;
+
+public enum ActivityType {
+    NIGHTOWL,
+    EARLYBIRD,
+    GAMER,
+    SINGER,
+    ARTIST,
+    SPORTSMAN,
+    GYMLOVER,
+    FASTFOOD,
+    HEALTHY,
+    VEGAN,
+    NERD,
+    EXTROVERT,
+    INTROVERT,
+    OFFICEWORKER,
+    STUDENT
+}
